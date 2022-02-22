4c4052320d9ddf6a911ef312b7854eb45680a2a8
carolinaltorres <carolinadelimatorres@gmail.com>
2022-02-22 11:03:29 -0300

    @carolinaltorres [Task:Criando Spring Initializr]

---
+++ aGenTIMove/src/main/java/com/agentimove/aGenTIMove/AGenTiMoveApplication.java
@@ -0,0 +1,13 @@
+package com.agentimove.aGenTIMove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AGenTiMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AGenTiMoveApplication.class, args);
+	}
+
+}
