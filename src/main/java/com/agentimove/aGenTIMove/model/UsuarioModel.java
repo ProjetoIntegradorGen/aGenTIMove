2762b924576467b7b997e28e6d10b370b27d1c4a
igorgll <igorlimagn@gmail.com>
2022-02-25 11:01:49 -0300

    @igorgll [Task: Creating CategoriaModel and setting Getters and Setters]

---
+++ src/main/java/com/agentimove/aGenTIMove/model/UsuarioModel.java
@@ -0,0 +1,60 @@
+package com.agentimove.aGenTIMove.model;
+
+import javax.validation.constraints.NotNull;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "tb_usuario")
+
+public class UsuarioModel {
+       
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @NotNull
+    private String nome;
+    
+    @NotNull
+    private String email;
+    
+    @NotNull
+    private String senha;
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    } 
+}
