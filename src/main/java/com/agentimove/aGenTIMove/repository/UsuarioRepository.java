553d5d8b28e01aec481ce27808c426528ce5f707
carolinaltorres <carolinadelimatorres@gmail.com>
2022-03-08 11:20:55 -0300

    @carolinaltorres [Task: Adding UserDetailsServiceImpl]

--- src/main/java/com/agentimove/aGenTIMove/repository/UsuarioRepository.java
+++ src/main/java/com/agentimove/aGenTIMove/repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package com.agentimove.aGenTIMove.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +7,5 @@ import com.agentimove.aGenTIMove.model.UsuarioModel;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
 	public Optional<UsuarioModel> findByNome(String nome);
-
-	//public List<UsuarioModel> findByNome(String nome);
-
+	public Optional<UsuarioModel> findByEmail(String email);
 }
