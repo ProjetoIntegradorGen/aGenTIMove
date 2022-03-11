79f879a1a24c406c3130318122e3dad8c7e6d2b6
Luiz Felipe Henrique Pereira <lfhenrique95@gmail.com>
2022-03-11 15:22:26 -0300

    @luizhenrique [Task:Created e Modified Controller]

--- src/main/java/com/agentimove/aGenTIMove/controller/ComprasController.java
+++ src/main/java/com/agentimove/aGenTIMove/controller/ComprasController.java
@@ -5,27 +5,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.agentimove.aGenTIMove.model.ComprasModel;
 import com.agentimove.aGenTIMove.repository.ComprasRepository;
+import com.agentimove.aGenTIMove.repository.ProdutoRepository;
+import com.agentimove.aGenTIMove.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/compras")
 @CrossOrigin("*")
 public class ComprasController {
-	@Autowired
-	public 	ComprasRepository repository;
-	
+
+	public @Autowired ComprasRepository repository;
+	public @Autowired UsuarioRepository repositoryU;
+	public @Autowired ProdutoRepository repositoryP;
+
 	@GetMapping("/{id}")
-	public ResponseEntity<ComprasModel>getById(@PathVariable long id){
-	return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
-	.orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<ComprasModel> getById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
-	// @GetMapping("/produtos/{produto}")
-	// public ResponseEntity<ComprasModel>getByProduto(@PathVariable String produto){
-	// 	return repository.findAllByProdutoContainingIgnoreCase(produto).map(resp -> ResponseEntity.ok(resp))
-	// 	.orElse(ResponseEntity.notFound().build());
-	// }
+
+	@PostMapping
+	public ResponseEntity<ComprasModel> novaCompra(@RequestBody ComprasModel compra) {
+		if (repositoryU.existsById(compra.getUsuario().getId())
+				&& repositoryP.existsById(compra.getProduto().getId())) {
+			return ResponseEntity.ok(repository.save(compra));
+		}
+		return ResponseEntity.badRequest().build();
+	}
+
 }
--- src/main/java/com/agentimove/aGenTIMove/repository/UsuarioRepository.java
+++ src/main/java/com/agentimove/aGenTIMove/repository/UsuarioRepository.java
@@ -1,11 +1,12 @@
 package com.agentimove.aGenTIMove.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.agentimove.aGenTIMove.model.UsuarioModel;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
-	public Optional<UsuarioModel> findByNome(String nome);
+	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String nome);
 	public Optional<UsuarioModel> findByEmail(String email);
 }
